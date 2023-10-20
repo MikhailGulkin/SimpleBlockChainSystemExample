14a7f18bf5917e2e40f9892ac974ccdd6e35e24e
Bol4onok <what@about.com>
2023-10-21 01:04:59 +0300

    pub/priv key

--- cmd/main.go
+++ cmd/main.go
@@ -14,5 +14,29 @@ func main() {
 	server := api.NewServer(handlers)
 	server.SetupRoutes()
 	server.Run()
-	
+
+	//privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	//signature := GenerateSignature(privateKey)
+	//m, _ := json.Marshal("1")
+	//h := sha256.Sum256(m)
+	//fmt.Println(ecdsa.Verify(&privateKey.PublicKey, h[:], signature.R, signature.S))
 }
+
+//type Signature struct {
+//	R *big.Int
+//	S *big.Int
+//}
+//
+//func GenerateSignature(private *ecdsa.PrivateKey) *Signature {
+//	m, _ := json.Marshal("1")
+//
+//	log.Println("Generate signature", string(m))
+//
+//	h := sha256.Sum256([]byte(m))
+//	r, s, err := ecdsa.Sign(rand.Reader, private, h[:])
+//	if err != nil {
+//		fmt.Println("Signing signature failed: ", err)
+//		return nil
+//	}
+//	return &Signature{R: r, S: s}
+//}
